05ae460f5572477cfb5f3c4b56b32c402a13a3c1
Julio-Martel <julioramdom123@gmail.com>
2026-03-07 18:26:30 -0300

    se ha creado el modulo

---
+++ tareas.js
@@ -0,0 +1,22 @@
+const tareas = [
+    {
+        codigo: 1,
+        tarea: 'Reposicion de gondolas',
+        descripcion: 'Colocar productos en gondolas'
+    },
+    
+    {
+        codigo: 2,
+        tarea: 'Cajero',
+        descripcion: 'Atencion al publico y cobrar productos' 
+    },
+
+    {
+        codigo: 3,
+        tarea: 'Limpieza',
+        descripcion: 'Limpieza de pasillos del mercado'
+    }
+
+];
+
+module.exports = tareas;
