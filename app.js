24e80d6a493c690fc985bce4c42a9886a27acc51
Julio-Martel <julioramdom123@gmail.com>
2026-03-08 18:07:38 -0300

    se ha creado con exito la verificacion de codigo mediante el uso del middleware

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 const express = require("express");
+const app = express();
+const tareas = require("./tareas/tareas");
+
+app.use(express.json());
+
+app.get('/tareas',(req,res) => {
+    res.json(tareas);
+})
+
+const tareaRegistrada = (req,res,next) => {
+
+    const codigo = parseInt(req.body.codigo);
+
+    const existe = tareas.find(t => t.codigo === codigo);
+
+    if(existe){
+        return res.status(409).json({
+            error: "Ese código ya existe"
+        });
+    }
+
+    next();
+
+}
+
+app.post('/tareas', tareaRegistrada, (req,res)=>{
+
+    const tareaNueva = {
+        codigo: tareas.length + 1,
+        tarea: req.body.tarea,
+        descripcion: req.body.descripcion
+    }
+
+    tareas.push(tareaNueva);
+
+    res.status(201).json(tareaNueva);
+
+});
+
+
+
+app.listen(3000,()=>{
+    console.log('Servidor en funcionamiento')
+})
